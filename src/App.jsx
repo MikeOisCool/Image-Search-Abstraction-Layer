3d5dd2bf4f5b88e3758241661f29fab44f46e3e3
Michael Ouazzani <morphois77@web.de>
2025-06-08 11:47:40 +0200

    css angepasst

--- src/App.jsx
+++ src/App.jsx
@@ -48,74 +48,78 @@ function App() {
       });
   }, [])
 
-  
+
 
   return (
 
     <div className="site" >
-      <h1>Image Search</h1>
-      <br />
-      {fallbackActive && (
-        <div style={{ color: 'orange', marginBottom: '1em' }}>
-          ⚠️ Es werden aktuell Fallback-Bilder angezeigt, da die API nicht erreichbar ist. 502 Bad Gateway!
-        </div>)}
-      <label >Gib eine Seitenummer ein:&nbsp;
-        <input
-          className="nummerFeld"
-          type="number"
-          min="1"
-          value={eingabeSeite}
-          onChange={(e) => {
-            const val = Number(e.target.value)
-            if (val >= 1) {
-              setEingabeSeite(val)
-            } else if (e.target.value === "") {
-              setEingabeSeite('')
-            }
-          }} />
-      </label>
-      <br />
-      <br />
-      <label>Gib deine Suche ein:     &nbsp;
-        <input value={suche} onChange={(e) => setSuche((e.target.value))} />
-      </label>
-     
-      
-      {/* Recent Searches  */}
       <div>
-        <h2 className='research' onClick= {() => {setIsExpanded(!isExpanded)}}>Hier klicken! Letzte Suchanfragen! Hier klicken!</h2>
-        {isExpanded && <ul>
-          {recentSearches.map((item, index) => (
-            <li key={index}>
-              {item.term} ({item.date ? new Date(item.date).toLocaleString(): 'Kein Datum'})
+        <h1>Image Search</h1>
+        <br />
+        {fallbackActive && (
+          <div style={{ color: 'orange', marginBottom: '1em' }}>
+            ⚠️ Es werden aktuell Fallback-Bilder angezeigt, da die API nicht erreichbar ist. 502 Bad Gateway!
+          </div>)}
+        <label >Gib eine Seitenummer ein:&nbsp;
+          <input
+            className="nummerFeld"
+            type="number"
+            min="1"
+            value={eingabeSeite}
+            onChange={(e) => {
+              const val = Number(e.target.value)
+              if (val >= 1) {
+                setEingabeSeite(val)
+              } else if (e.target.value === "") {
+                setEingabeSeite('')
+              }
+            }} />
+        </label>
+        <br />
+        <br />
+        <label>Gib deine Suche ein:     &nbsp;
+          <input value={suche} onChange={(e) => setSuche((e.target.value))} />
+        </label>
 
-            </li>
+
+        {/* Recent Searches  */}
+        <div>
+          <h2 className='research' onClick={() => { setIsExpanded(!isExpanded) }}>Hier klicken! Letzte Suchanfragen! Hier klicken!</h2>
+          <div className={`recentSearchesContainer ${isExpanded ? 'expanded' : ''}`}>
+            <ul>
+              {recentSearches.map((item, index) => (
+                <li key={index}>
+                  {item.term} ({item.date ? new Date(item.date).toLocaleString() : 'Kein Datum'})
+
+                </li>
+              ))}
+            </ul></div>
+        </div>
+
+        {/* Bilder */}
+        <div className="pics">
+
+          {images.map((image, index) => (
+            <div key={index} className='pic'>
+              <a href={image.url} target="_blank" rel="noreferrer" title="Bild in voller Größe ansehen">
+                <img
+                  src={image.thumbnail?.url || image.url}
+                  alt={image.description || "Bild"}
+                  width={150}
+                  height={150}
+                  onError={(e) => {
+                    e.target.onerror = null;
+                    e.target.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Cat_crying_%28Lolcat%29.jpg/960px-Cat_crying_%28Lolcat%29.jpg'
+                  }}
+                />
+              </a>
+            </div>
           ))}
-        </ul>}
-      </div>
 
-      {/* Bilder */}
-      <div className="pics">
-
-        {images.map((image, index) => (
-          <div key={index} className='pic'>
-            <a href={image.url} target="_blank" rel="noreferrer" title="Bild in voller Größe ansehen">
-              <img
-                src={image.thumbnail?.url || image.url}
-                alt={image.description || "Bild"}
-                width={150}
-                height={150}
-                onError={(e) => {
-                  e.target.onerror = null;
-                  e.target.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Cat_crying_%28Lolcat%29.jpg/960px-Cat_crying_%28Lolcat%29.jpg'
-                }}
-              />
-            </a>
-          </div>
-        ))}
-
+        </div>
       </div>
     </div>
+
 
   )
 }
